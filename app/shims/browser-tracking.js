158e5afa4e77e940ab70777f7d7a03f0f7282ae7
vitaliy <vitaliy.tarash@gmail.com>
2016-01-09 00:16:13 +0300

    some additional styles for ie

--- app/shims/browser-tracking.js
+++ app/shims/browser-tracking.js
@@ -1,3 +1,4 @@
+//firefox
 if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
 	navigator.isFF = true;
 	
@@ -6,4 +7,17 @@ if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
 		evt.initMouseEvent('click', true, true, this.ownerDocument.defaultView, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
 		this.dispatchEvent(evt);
 	}
+
+}
+
+//ie
+var detectIEregexp;
+if (navigator.userAgent.indexOf('MSIE') != -1)
+	detectIEregexp = /MSIE (\d+\.\d+);/ //test for MSIE x.x
+else // if no "MSIE" string in userAgent
+	detectIEregexp = /Trident.*rv[ :]*(\d+\.\d+)/ //test for rv:x.x or rv x.x where Trident string exists
+
+if (detectIEregexp.test(navigator.userAgent)){ //if some form of IE
+	var ieversion=new Number(RegExp.$1) // capture x.x portion and store as a number
+	document.body.className += ' ie ie-'+ieversion;
 }
